afadddd7475e14b2a7dd7573795337eed1abcc09
eethalteam <eethalnaditsolutions@gmail.com>
2026-01-06 15:52:50 +0530

    Changes in Country

--- models/masterModels/Country.js
+++ models/masterModels/Country.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Define the City Schema
 const CountrySchema = new mongoose.Schema(
@@ -6,33 +6,34 @@ const CountrySchema = new mongoose.Schema(
     CountryCode: {
       type: String,
       required: true,
-      
-      trim: true
+
+      trim: true,
     },
     CountryName: {
       type: String,
       required: true,
-      
-      trim: true
+
+      trim: true,
     },
-    StateID: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'State', 
-        required: true
-      },
+    // StateID: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'State',
+    //     required: true
+    //   },
     description: {
       type: String,
       trim: true,
-      maxlength: [500, 'Description cannot exceed 500 characters']
+      maxlength: [500, "Description cannot exceed 500 characters"],
     },
     isActive: {
       type: Boolean,
-      default: true
-    }
-  }, { timestamps: true }
+      default: true,
+    },
+  },
+  { timestamps: true }
 );
 
 // Create the Country Model
-const Country = mongoose.model('Country', CountrySchema);
+const Country = mongoose.model("Country", CountrySchema);
 
 module.exports = Country;
